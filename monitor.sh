03d9fb99f30cde13d8c4b3a24718f137f572c060
allevs01 <stefanoallevi01@gmail.com>
2024-07-31 12:48:31 +0200

    monitor.sh modified

--- monitor.sh
+++ monitor.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-# Avviare il programma Rust in background
-./10claim &
+# Numero di esecuzioni desiderate
+N=20
 
-# PID del programma Rust
-PID=$!
+# File di output per la raccolta dei dati CPU
+CPU_USAGE_FILE="cpu_usage.txt"
 
-# Raccolta dati CPU con top
-top -b -d 1 -n 10 > cpu_usage.txt &
+# Inizializzare il file di output (opzionale, per rimuovere dati precedenti)
+echo "Raccolta dati CPU" > $CPU_USAGE_FILE
 
-# Attendere che il programma Rust termini
-wait $PID
+# Avviare il programma Rust e raccogliere i dati per N volte
+for ((i=1; i<=N; i++))
+do
+    # Eseguire il programma Rust in background
+    ./10claim &
+
+    # PID del programma Rust
+    PID=$!
+
+    # Raccolta dati CPU con top in modalità batch, appending al file di output
+    echo "Esecuzione $i" >> $CPU_USAGE_FILE
+    top -b -d 1 -n 10 >> $CPU_USAGE_FILE
+
+    # Attendere che il programma Rust termini
+    wait $PID
+
+    echo "Esecuzione $i completata."
+done
 
 echo "Raccolta dati completata."
